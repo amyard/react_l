aae31647982e6890d9a884c7adb42ab5cfa4404c
mturenko <maksymturenko@gmail.com>
2022-08-01 14:52:04 +0300

    add component - Product

--- minimal_pr/src/App.tsx
+++ minimal_pr/src/App.tsx
@@ -1,23 +1,35 @@
 import React, {createElement as e, useState} from 'react';
 import logo from './logo.svg';
+import {Product} from './components/Product';
+import {products} from './data/products';
 
 function App() {
-  // return (
-  //   <h1>Hello React.</h1>
-  // );
-
   //return e('h1', {}, 'Hello from JS');
 
-  const [count, setCount] = useState(0);
+  // old syntax
+  // const [count, setCount] = useState(0);
 
-  return e("div", {className:"container"}, [
-    e('h1', {className:'font-bold', key: 1}, `Test JSX ${count}`),
-    e('button', {
-      className:'py2 px-4 border', 
-      key: 2,
-      onClick: () => setCount(count+1)
-    }, 'Click me.'),
-  ])
+  // return e("div", {className:"container"}, [
+  //   e('h1', {className:'font-bold', key: 1}, `Test JSX ${count}`),
+  //   e('button', {
+  //     className:'py2 px-4 border', 
+  //     key: 2,
+  //     onClick: () => setCount(count+1)
+  //   }, 'Click me.'),
+  // ])
+
+  // jsx syntax
+  return (
+    <div className='container mx-auto max-w-2xl pt-5'>
+      {/* <Product 
+        product={products[0]} 
+        title="test1 syntax" 
+        title2={'test2 syntax'} /> */}
+
+    <Product product={products[0]} />
+    <Product product={products[1]} />
+    </div>
+  );
 }
 
 export default App;
---
+++ minimal_pr/src/components/Product.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { IProduct } from '../models';
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product(props: ProductProps) {
+    return (
+        <div className='border py-2 px-4 rounded flex flex-col items-center mb-2'>
+            { props.product.title }
+        </div>
+    );
+}
