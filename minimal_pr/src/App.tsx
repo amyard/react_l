73d64701179b05e182a0bf6530193f9d76d1a453
mturenko <maksymturenko@gmail.com>
2022-08-01 14:20:47 +0300

    old version of creating components

--- minimal_pr/src/App.tsx
+++ minimal_pr/src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {createElement as e, useState} from 'react';
 import logo from './logo.svg';
 
 function App() {
-  return (
-    <h1>Hello React.</h1>
-  );
+  // return (
+  //   <h1>Hello React.</h1>
+  // );
+
+  //return e('h1', {}, 'Hello from JS');
+
+  const [count, setCount] = useState(0);
+
+  return e("div", {className:"container"}, [
+    e('h1', {className:'font-bold', key: 1}, `Test JSX ${count}`),
+    e('button', {
+      className:'py2 px-4 border', 
+      key: 2,
+      onClick: () => setCount(count+1)
+    }, 'Click me.'),
+  ])
 }
 
 export default App;
