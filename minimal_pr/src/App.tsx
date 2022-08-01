25bdff40aaf392e2038da34e42f3fc5801f13daa
mturenko <maksymturenko@gmail.com>
2022-08-01 17:06:02 +0300

    add addtional useState for loading and error handler

--- minimal_pr/src/App.tsx
+++ minimal_pr/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {createElement as e, useEffect, useState} from 'react';
 import {Product} from './components/Product';
 // import {products} from './data/products';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { IProduct } from './models';
 
 function App() {
@@ -23,9 +23,27 @@ function App() {
   // [products, setProducts] - products - first position, setProducts - second action after running
   const [products, setProducts] = useState<IProduct[]>([]);
 
+  // if start add - show loading, after getting data - remove loading
+  const [loading, setLoading] = useState(false);
+
+  const [error, setError] = useState('');
+
   async function fetchProducts() {
-    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
-    setProducts(response.data);
+    try {
+      setError('');
+
+      // change loading to true
+      setLoading(true);
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+      setProducts(response.data);
+
+      // change loading to false
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
   }
 
   // если передила [] то он вызовется только один раз, когда реакт будет готов
@@ -36,13 +54,16 @@ function App() {
   // jsx syntax
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-    {products.map(product =><Product product={product} key={product.id} />)}
-    
-    {/* not use such syntax
-    {products.map((product, index) =><Product product={product} key={index} />)} */}
+      {loading && <div className='text-center'>Loading...</div>}
+      {error && <div className='text-center text-red-600'>{error}</div>}
 
-    {/* <Product product={products[0]} />
-    <Product product={products[1]} /> */}
+      {products.map(product =><Product product={product} key={product.id} />)}
+      
+      {/* not use such syntax
+      {products.map((product, index) =><Product product={product} key={index} />)} */}
+
+      {/* <Product product={products[0]} />
+      <Product product={products[1]} /> */}
     </div>
   );
 }
