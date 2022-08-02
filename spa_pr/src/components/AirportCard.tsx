e2e3b1a0138fc47e983474346e3141ca28780c90
mturenko <maksymturenko@gmail.com>
2022-08-02 14:04:29 +0300

    main page - add components

---
+++ spa_pr/src/components/AirportCard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export function AirportCard(){
+    return (
+        <div>cart</div>
+    );
+}
--- spa_pr/src/pages/MainPage.tsx
+++ spa_pr/src/pages/MainPage.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { AirportCard } from '../components/AirportCard';
+import { AirportFilter } from '../components/AirportFilter';
+import { AirportSearch } from '../components/AirportSearch';
 
 
 export function MainPage() {
     return (
-        <div>Main page</div>
+        <div className='container mx-auto max-w-[760px] pt-5'>
+            <AirportSearch />
+            <AirportFilter />
+            <AirportCard />
+        </div>
     );
 }
