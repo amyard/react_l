1554090dca3d6ffb5b19fa59042b3934492bb82f
Maksym Turenko <maksym@bundledocs.com>
2023-03-02 21:10:56 +0000

    stop

---
+++ meals/src/context.jsx
@@ -0,0 +1,9 @@
+﻿const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+    return <AppContext.Provider value="hello">
+        {children}
+    </AppContext.Provider>
+};
+
+export {AppContext, AppProvider};
