9b65c28ce16bc5dfaa537f067835762262101ec2
Maksym Turenko <maksym@bundledocs.com>
2023-03-04 20:28:09 +0000

    get data with redux

---
+++ redux-ts/src/hooks/useActions.ts
@@ -0,0 +1,8 @@
+﻿import {useDispatch} from "react-redux";
+import {bindActionCreators} from "redux";
+import {actionCreators} from '../state'
+
+export const useActions = () => {
+  const dispatch = useDispatch();
+  return bindActionCreators(actionCreators, dispatch);
+};
