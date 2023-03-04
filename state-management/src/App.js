5bf9c192917c8a3d06526ec46a8aec9688f7d62c
Maksym Turenko <maksym@bundledocs.com>
2023-03-04 23:22:36 +0000

    useReducer

--- state-management/src/App.js
+++ state-management/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {useReducer, useState} from "react";
 
 
 /*  useState section  */
@@ -47,11 +46,72 @@ function Counter() {
 }
 /*  useState section  */
 
+
+/*  useReducer section  */
+function SomeReducer() {
+    const [state, dispatch] = useReducer((state, action) => {
+        switch (action.type) {
+            case "SET_NAME":
+                return {...state, name: action.payload};
+            case "ADD_NAME":
+                return {
+                    ...state, 
+                    names: [...state.names, state.name], 
+                    name:""
+                };
+        }
+    }, {
+        names: [],
+        name: "",
+    });
+    
+    return (
+        <div>
+            <div>
+                {
+                    state.names.map((name, index) => (
+                        <div key={index}>{name}</div>
+                    ))
+                }
+            </div>
+            <input 
+                type="text"
+                value={state.name}
+                onChange={e => dispatch({type:"SET_NAME", payload: e.target.value})}
+            />
+            <button onClick={() => dispatch({type:"ADD_NAME"})}>Add Name</button>
+            <div>Name = {state.name}</div>
+        </div>
+    )
+}
+
+function UserForm() {
+    const [state, dispatch] = useReducer((state, action) => ({
+        ...state,
+        ...action
+    }), {
+        first: "",
+        last: "",
+    })
+    
+    return (
+        <div>
+            <input type="text" value={state.first} onChange={(e) => dispatch({first: e.target.value})} />
+            <input type="text" value={state.last} onChange={(e) => dispatch({last: e.target.value})} />
+            <div>First - {state.first}. Last - {state.last}</div>
+        </div>
+    )
+}
+/*  useReducer section  */
+
 function App() {
   return (
       <div>
           <Counter />
           <NameList />
+          <hr/>
+          <SomeReducer />
+          <UserForm />
       </div>
   )
 }
