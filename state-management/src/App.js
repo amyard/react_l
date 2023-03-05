ca9ab338e3b5c778dcfdba942a6a142f0356a27c
Maksym Turenko <maksym@bundledocs.com>
2023-03-05 13:49:20 +0000

    useCallback

--- state-management/src/App.js
+++ state-management/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useMemo, useReducer, useState} from "react";
+import {useCallback, useMemo, useReducer, useState} from "react";
 
 
 /*  useState section  */
@@ -139,22 +139,31 @@ function ListOfNumbersUseMemo() {
         </>
     )
 }
-function SortedListForUseCallback({list}) {
+function SortedListForUseCallback({list, sortFunc}) {
     // runned as many as current component if rendering
     console.log("SortedListForUseCallback render.") 
     
     const sortedList = useMemo(() => {
         // will run only twice --> because of using react 18
         console.log("Running sort");
-        return [...list].sort();
-    }, [list]);
+        return [...list].sort(sortFunc);
+    }, [list, sortFunc]);
     return <div>{sortedList.join(", ")}</div>
 }
 
 function SortListForUseCallback() {
+    const [c3, setC3] = useState(0);
     const [names] = useState(["max","alex","lol","awe"]);
     
-    return <><SortedListForUseCallback list={names} /></>
+    // if we need to create a sorted array which didn't change to reduce amount of calls
+    const sortFunc = useCallback((a, b) => a.localeCompare(b), []);
+    // inverse sorting
+    // const sortFunc = (a, b) => a.localeCompare(b) * -1;
+    
+    return <>
+        <button onClick={() => setC3(c3 + 1)}>Click me</button>
+        <SortedListForUseCallback list={names} sortFunc={sortFunc} />
+    </>
 }
 /*  monitoring state section  */
 
